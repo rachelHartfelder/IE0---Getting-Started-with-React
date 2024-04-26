298f584f9f880c833d2b30b674807b5221466c76
rachelHartfelder <113880300+rachelHartfelder@users.noreply.github.com>
2024-04-26 09:38:31 -0700

    removed user list from backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -8,31 +8,7 @@ const port = 8000;
 
 const users = {
     users_list: [
-        {
-            id: "xyz789",
-            name: "Charlie",
-            job: "Janitor"
-        },
-        {
-            id: "abc123",
-            name: "Mac",
-            job: "Bouncer"
-        },
-        {
-            id: "ppp222",
-            name: "Mac",
-            job: "Professor"
-        },
-        {
-            id: "yat999",
-            name: "Dee",
-            job: "Aspring actress"
-        },
-        {
-            id: "zap555",
-            name: "Dennis",
-            job: "Bartender"
-        }
+        
     ]
 };
 
